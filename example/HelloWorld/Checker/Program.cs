57a1edf0414560ed22f7b732dd042bb6effb1e6f
eden <a@a.a>
2024-03-17 19:30:07 +0200

    added c# checker script

---
+++ example/HelloWorld/Checker/Program.cs
@@ -0,0 +1,35 @@
+﻿
+public class Program {
+
+    private static void Main(string[] args) {
+
+
+        // Getting the input from alice. (crash dir & stdout file)
+
+        using (var stream = File.AppendText("./alice_checker_input.txt")) {
+
+            stream.WriteLine("======== START ========");
+
+            foreach (string arg in args) { 
+
+                stream.WriteLine(arg);
+            }
+
+            stream.WriteLine("========= END =========" + "\n\n");
+        }
+
+
+        // Saving the stdout to a file.
+
+        using (var stream = File.AppendText("./alice_checker_stdout_received.txt")) {
+
+            string alice_output = File.ReadAllText(args[1]);
+
+            stream.WriteLine("======== START ========");
+
+            stream.WriteLine(alice_output);
+
+            stream.WriteLine("========= END =========" + "\n\n");
+        }
+    }
+}
